9bc753f475a5b0f7465ca6b3e9941f0f9a198b65
Ian Yin <xinghuan.yin@gmail.com>
2026-08-06 22:12:10 +0800

    add: City of Tides shared-world experiment

--- add-categories.py
+++ add-categories.py
@@ -38,6 +38,7 @@ ALLOWED = {
 
 # Canonical mapping. Keys are game ids exactly as they appear in games.json.
 CATEGORIES = {
+    'city-of-tides': 'social',
     'the-wild-road': 'strategy',
     'rooftop-apartment': 'strategy',
     'seventh-dock': 'strategy',
